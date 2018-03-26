59d1864c7951320d55dbdfe788b34c5010d76cfa
Churuu <robbcap1@gmail.com>
2018-03-26 14:33:10 +0200

    no message

--- Horror-Game_Proj/Assets/_Scripts/Misc_/Generator.cs
+++ Horror-Game_Proj/Assets/_Scripts/Misc_/Generator.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generator : MonoBehaviour {
+public class Generator : MonoBehaviour 
+{
 
+	GameObject particleSmoke;
 	public bool generatorIsOn = false;
+
+	public void EnableGenerator()
+	{
+		generatorIsOn = true;
+		//particleSmoke.SetActive(true);
+	}
 }
+
+
