a28154f8d0d5b8b1811fb6a68c641ff31a690176
Churu <git@bitbucket.org:Churuu/unity.git>
2018-03-28 16:00:59 +0200

    no message

---
+++ Horror-Game_Proj/Assets/_Scripts/Player/GeigerCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeigerCounter : MonoBehaviour
+{
+    AudioSource audioSrc;
+	public AudioClip geigerClick;
+    public float intervalCycle;
+	public float intervalFrequency;
+    float interval;
+
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        interval = intervalCycle + Time.time;
+    }
+
+    void Update()
+    {
+        if (Time.time > interval)
+        {
+			audioSrc.PlayOneShot(geigerClick);
+            interval = Mathf.PerlinNoise(intervalCycle, intervalFrequency) + Time.time;
+        }
+    }
+}
