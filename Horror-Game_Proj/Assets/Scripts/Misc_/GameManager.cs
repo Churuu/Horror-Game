460ebdb189e9dec88805c0a93a46b9be27c31b32
Churu <git@bitbucket.org:Churuu/unity.git>
2018-03-29 15:17:45 +0200

    Optimisering av kod, bättre flow i animationer och mindre fails, bättre interaction mellan föremål

---
+++ Horror-Game_Proj/Assets/Scripts/Misc_/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public string sceneToSwitch;
+    public AnimationClip fadeAnimation;
+    public Animator _anim;
+
+
+    void Start()
+    {
+
+    }
+
+    public void EndGame()
+    {
+        _anim.SetTrigger("Fade");
+        Invoke("SwitchScene", fadeAnimation.length + 1);
+    }
+
+    private void SwitchScene()
+    {
+        FindObjectOfType<PlayerController>().UnlockCursor();
+        SceneManager.LoadScene(sceneToSwitch);
+    }
+}
---
+++ Horror-Game_Proj/Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+
+    [HideInInspector] public List<GameObject> inventory = new List<GameObject>();
+
+
+    public void Add(GameObject obj)
+    {
+        inventory.Add(obj);
+    }
+
+    public bool Contains(GameObject obj)
+    {
+        if (inventory.Contains(obj))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
