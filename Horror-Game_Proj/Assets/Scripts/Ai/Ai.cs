16a716c458ae02d1167e0292de5ec480f29986dc
Churu <git@bitbucket.org:Churuu/unity.git>
2018-04-17 11:01:16 +0200

    no message

--- Horror-Game_Proj/Assets/Scripts/Ai/Ai.cs
+++ Horror-Game_Proj/Assets/Scripts/Ai/Ai.cs
@@ -17,7 +17,7 @@ public class Ai : MonoBehaviour
     [Range(1, 10)] public float walkingSpeed;
     [Range(100, 180)] public int fieldOfViewAngle;
     public Image fade;
-    public AudioClip[] sounds; // 0 Walking, 
+    public AudioClip[] sounds; // 0 Walking, 1 Scary angry sound
     [HideInInspector] public List<Transform> aiPointToPatrol;
     [HideInInspector] public bool playerIsVisible = false;
     [HideInInspector] public GameObject currentFloorToPatrol;
@@ -36,6 +36,7 @@ public class Ai : MonoBehaviour
     private GameObject[] roomHolder;
     private float stepCycle = .7f;
     private float stepCycleCounter;
+    private float soundCycleCounter;
     private AudioSource src;
 
 
@@ -49,6 +50,7 @@ public class Ai : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         _anim = GetComponent<Animator>();
         stepCycleCounter = Time.time + stepCycle;
+        soundCycleCounter = Time.time + UnityEngine.Random.Range(30, 300);
         src = GetComponent<AudioSource>();
 
     }
@@ -79,6 +81,8 @@ public class Ai : MonoBehaviour
                 ChasePlayer();
                 break;
         }
+
+        PlaySounds();
     }
 
     void AnimationHandler()
@@ -137,10 +141,6 @@ public class Ai : MonoBehaviour
         if (!agent.pathPending)
             if (agent.remainingDistance <= 0.1f)
                 Invoke("ReturnToRoomPatrol", 1);
-
-
-
-
     }
 
     public void PatrolRoom()
@@ -213,7 +213,6 @@ public class Ai : MonoBehaviour
         {
             System.Random rnd = new System.Random();
             List<Transform> ScrambledPatrolPoints = aiPointToPatrol.OrderBy(x => rnd.Next()).ToList();
-
             this.aiPointToPatrol = ScrambledPatrolPoints;
             patrollingPoint = 0;
             state = AiState.patrolKeypoint;
@@ -241,13 +240,21 @@ public class Ai : MonoBehaviour
     }
 
 
-
     void StepSound()
     {
         if (Time.time > stepCycleCounter && agent.velocity.magnitude > 0)
         {
             stepCycleCounter = Time.time + stepCycle;
             src.PlayOneShot(sounds[0], 1);
+        }
+    }
+
+    void PlaySounds()
+    {
+        if (Time.time > soundCycleCounter)
+        {
+            soundCycleCounter = Time.time + UnityEngine.Random.Range(30, 300);
+            src.PlayOneShot(sounds[UnityEngine.Random.Range(1, 4)], 1);
         }
     }
 }
