ce301c0430fb3e9294a5abfe13ee9f37e3fc019d
Churu <git@bitbucket.org:Churuu/unity.git>
2018-04-27 11:47:11 +0200

    no message

--- Horror-Game_Proj/Assets/Scripts/Misc_/Paper.cs
+++ Horror-Game_Proj/Assets/Scripts/Misc_/Paper.cs
@@ -5,17 +5,20 @@ using UnityEngine.UI;
 
 public class Paper : MonoBehaviour
 {
-	public Image paper;
-	public void ShowPaper()
-	{
-		paper.enabled = true;
-		gameObject.SetActive(false);
-		Invoke("HidePaper", 2);
-	}
+    public Image paper;
+    public void ShowPaper()
+    {
+        paper.enabled = true;
+        gameObject.SetActive(false);
+        InvokeRepeating("HidePaper", 0, Time.deltaTime);
+    }
 
-	void HidePaper()
-	{
-		paper.enabled = false;
-		Destroy(gameObject);
-	}
+    void HidePaper()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paper.enabled = false;
+            Destroy(gameObject);
+        }
+    }
 }
