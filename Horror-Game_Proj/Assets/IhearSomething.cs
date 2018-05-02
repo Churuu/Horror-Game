5b2d54a6a7a6d5ff907f815a3c8f37cf86b5ccb7
DIHFrosche <35454882+DIHFrosche@users.noreply.github.com>
2018-05-02 10:52:33 +0200

    no message

---
+++ Horror-Game_Proj/Assets/IhearSomething.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IhearSomething : MonoBehaviour {
+
+    public AudioClip sound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<AudioSource>().PlayOneShot(sound);
+    }
+}
